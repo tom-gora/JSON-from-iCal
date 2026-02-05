d7a8a163e8eff87fedf6d0cd4dd887e245d6f13b
tom-gora <goratomasz@outlook.com>
2026-02-05 21:11:10 +0000

    refine web demo real nice this time with some modern css and json data preview via prismjs

--- _web_showcase/src/pages/api/process-urls.ts
+++ _web_showcase/src/pages/api/process-urls.ts
@@ -1,18 +1,18 @@
-import type { APIRoute } from 'astro';
-import { runJfi } from '../../lib/jfi';
+import type { APIRoute } from "astro";
+import { runJfi } from "../../lib/jfi";
 
 export const POST: APIRoute = async ({ request }) => {
   try {
     const { urls, options } = await request.json();
     const data = runJfi(urls, options);
-    return new Response(JSON.stringify(data), { 
+    return new Response(JSON.stringify(data), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
   } catch (error: any) {
-    return new Response(JSON.stringify({ error: error.message }), { 
+    return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" },
     });
   }
 };
