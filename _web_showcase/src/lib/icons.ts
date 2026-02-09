ccb07d6b3dbf2bc29836d0f5144c58dd3977cff6
tom-gora <goratomasz@outlook.com>
2026-02-09 23:04:55 +0000

    CHORE: initial fixes to responsiveness. tidy up codebase to demonstrate maintainability. sources of truth: icons

---
+++ _web_showcase/src/lib/icons.ts
@@ -0,0 +1,84 @@
+export const ICONS = {
+  back: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 24 24" {...props}>
+	<path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20z" />
+</svg>`,
+
+  close: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 24 24" {...props}>
+<path fill="currentColor" d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12z" />
+</svg>`,
+
+  clear: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 14 14" {...props}>
+<path fill="currentColor" fill-rule="evenodd" d="M7.729.932A.75.75 0 0 0 6.273.568l-.48 1.922a.75.75 0 1 0 1.455.363zm-.306 3.392a.75.75 0 0 1 .75-.75h2.536a3.286 3.286 0 1 1 0 6.572H8.922a.75.75 0 0 1 0-1.5h1.787a1.786 1.786 0 0 0 0-3.572H8.173a.75.75 0 0 1-.75-.75m-3.6 2.265a.75.75 0 0 1 0 1.06L2.029 9.445a1.796 1.796 0 0 0 0 2.533a1.796 1.796 0 0 0 2.532 0v-.001l1.066-1.066a.75.75 0 1 1 1.06 1.061L5.62 13.04h-.001a3.295 3.295 0 0 1-4.651 0l-.003-.002a3.296 3.296 0 0 1 0-4.65l.001-.002L2.763 6.59a.75.75 0 0 1 1.06 0M.087 3.297a.75.75 0 0 1 1.006-.336l1.922.96a.75.75 0 1 1-.671 1.343L.422 4.303a.75.75 0 0 1-.336-1.006M3.83.415a.75.75 0 1 0-1.342.67l.96 1.922a.75.75 0 0 0 1.342-.671z" clip-rule="evenodd" />
+</svg>`,
+
+  moon: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 30 30" {...props}>
+<path fill="currentColor" d="M3.74 14.44c0-1.52.3-2.98.89-4.37s1.4-2.58 2.4-3.59s2.2-1.81 3.59-2.4s2.84-.89 4.37-.89s2.98.3 4.37.89s2.59 1.4 3.6 2.4s1.81 2.2 2.4 3.59s.89 2.84.89 4.37s-.3 2.98-.89 4.37s-1.4 2.59-2.4 3.6s-2.2 1.81-3.6 2.4s-2.85.89-4.37.89s-2.98-.3-4.37-.89s-2.58-1.4-3.59-2.4s-1.81-2.2-2.4-3.6s-.89-2.84-.89-4.37M15.15 4.39c1.83 3.22 2.74 6.57 2.74 10.05c0 4.16-.88 7.51-2.65 10.06c1.34-.03 2.61-.32 3.82-.86s2.25-1.27 3.13-2.16s1.57-1.95 2.09-3.18s.78-2.51.78-3.86c0-1.8-.44-3.46-1.33-5s-2.08-2.75-3.6-3.65s-3.18-1.37-4.98-1.4" />
+</svg>`,
+
+  cuppa: `<svg xmlns="http://www.w4.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 24 24" {...props}>
+<path fill="currentColor" d="M4 19h16v2H4zM20 8V5h-2v3zm0-5c.6 0 1 .2 1.4.6s.6.9.6 1.4v3c0 .6-.2 1-.6 1.4s-.8.6-1.4.6h-2v3c0 1.1-.4 2-1.2 2.8S15.1 17 14 17H8c-1.1 0-2-.4-2.8-1.2S4 14.1 4 13V3h5v2.4L7.2 6.8c-.1.1-.2.3-.2.4v4.3c0 .3.2.5.5.5h4c.3 0 .5-.2.5-.5V7.2c0-.2-.1-.3-.2-.4L10 5.4V3z" />
+</svg>`,
+
+  burger: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 48 48" {...props}>
+<path fill="currentColor" fill-rule="evenodd" d="M24.5 2.5c-2.617 0-4.902.043-6.882.113C10.359 2.868 4.5 8.506 4.5 15.875c0 1.727.194 3.218.406 4.336c.368 1.936 1.91 3.31 3.808 3.56c2.44.323 7.21.729 15.786.729s13.346-.406 15.786-.728c1.897-.251 3.44-1.625 3.808-3.561c.212-1.118.406-2.61.406-4.336c0-7.369-5.86-13.007-13.118-13.262c-1.98-.07-4.265-.113-6.882-.113m11 12.5a1.5 1.5 0 0 1 1.5-1.5h.5a1.5 1.5 0 0 1 0 3H37a1.5 1.5 0 0 1-1.5-1.5M31 9.5a1.5 1.5 0 0 0 0 3h.5a1.5 1.5 0 0 0 0-3zm1 8.5a1.5 1.5 0 0 0-1.5-1.5H30a1.5 1.5 0 0 0 0 3h.5A1.5 1.5 0 0 0 32 18m7.499 8.385c-2.781.304-7.46.615-14.999.615s-12.217-.31-14.999-.615c-2.337-.257-4.474-1.462-5.831-3.347q-.116.045-.23.097c-.753.345-1.502.986-1.78 2.051c-.102.394-.16.831-.16 1.314s.058.921.16 1.315c.278 1.065 1.027 1.706 1.78 2.051c.715.328 1.5.428 2.134.448c2.387.075 7.738.186 18.426.186c10.3 0 15.643-.103 18.154-.178c.737-.021 1.627-.15 2.417-.537c.831-.409 1.628-1.156 1.847-2.355q.08-.435.082-.93c.002-.495-.029-.64-.082-.93c-.18-.988-.754-1.67-1.416-2.107c-1.36 1.641-3.347 2.685-5.503 2.922M24 33c-10.703 0-16.081-.112-18.504-.188c-.799-.024-1.956-.15-3.098-.674l-.05-.023l-.008.006c-.283.215-.949.848-.825 1.898a2.91 2.91 0 0 0 1.304 2.134c.632.42 1.366.589 1.988.666c.983.122 2.14.052 2.995 0q.301-.019.542-.03c.974-.045 1.745.06 2.185.287c3.663 1.899 7.526 1.899 11.189 0c1.589-.824 2.977-.824 4.566 0c3.663 1.899 7.526 1.899 11.19 0c.438-.228 1.21-.332 2.184-.287q.242.011.542.03c.854.052 2.012.122 2.995 0c.622-.077 1.356-.247 1.987-.666a2.9 2.9 0 0 0 1.305-2.134c.107-.913-.391-1.522-.695-1.79a2 2 0 0 0-.204-.16c-1.207.572-2.454.725-3.36.752C39.684 32.896 34.315 33 24 33M8.46 39.286c.357-.017.643-.005.846.022q.107.015.146.025c4.363 2.235 9.056 2.222 13.416-.038c.867-.45 1.398-.45 2.265 0c4.36 2.26 9.053 2.273 13.417.038q.038-.01.145-.025c.203-.027.489-.039.847-.022l.396.023c.821.052 2.316.146 3.565-.01a9 9 0 0 0 .996-.18c-.074 3.85-3.739 6.063-6.959 6.18c-2.824.105-7.053.2-13.04.2s-10.215-.095-13.04-.2c-3.168-.116-6.768-2.26-6.952-5.998c1.247.154 2.736.06 3.556.008z" clip-rule="evenodd" />
+</svg>`,
+
+  info: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 640 1408" {...props}>
+	<path fill="currentColor" d="M640 1216v128q0 26-19 45t-45 19H64q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h64V768H64q-26 0-45-19T0 704V576q0-26 19-45t45-19h384q26 0 45 19t19 45v576h64q26 0 45 19t19 45M512 64v192q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V64q0-26 19-45t45-19h256q26 0 45 19t19 45" />
+</svg>`,
+
+  themeLight: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 24 24" {...props}>
+	<g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+		<path d="M12 6c3.31 0 6 2.69 6 6c0 3.31 -2.69 6 -6 6c-3.31 0 -6 -2.69 -6 -6c0 -3.31 2.69 -6 6 -6Z">
+			<animate fill="freeze" attributeName="d" dur="0.6s" values="M12 26c3.31 0 6 2.69 6 6c0 3.31 -2.69 6 -6 6c-3.31 0 -6 -2.69 -6 -6c0 -3.31 2.69 -6 6 -6Z;M12 6c3.31 0 6 2.69 6 6c0 3.31 -2.69 6 -6 6c-3.31 0 -6 -2.69 -6 -6c0 -3.31 2.69 -6 6 -6Z" />
+		</path>
+		<path d="M12 21v1M21 12h1M12 3v-1M3 12h-1" opacity="0">
+			<animateTransform attributeName="transform" dur="30s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
+			<set fill="freeze" attributeName="opacity" begin="0.7s" to="1" />
+			<animate fill="freeze" attributeName="d" begin="0.7s" dur="0.2s" values="M12 19v1M19 12h1M12 5v-1M5 12h-1;M12 21v1M21 12h1M12 3v-1M3 12h-1" />
+		</path>
+		<path d="M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5" opacity="0">
+			<animateTransform attributeName="transform" dur="30s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
+			<set fill="freeze" attributeName="opacity" begin="0.9s" to="1" />
+			<animate fill="freeze" attributeName="d" begin="0.9s" dur="0.2s" values="M17 17l0.5 0.5M17 7l0.5 -0.5M7 7l-0.5 -0.5M7 17l-0.5 0.5;M18.5 18.5l0.5 0.5M18.5 5.5l0.5 -0.5M5.5 5.5l-0.5 -0.5M5.5 18.5l-0.5 0.5" />
+		</path>
+	</g>
+</svg>`,
+
+  themeDark: `<svg xmlns="http://www.w3.org/2000/svg" style="width: var(--iconSize); height: var(--iconSize);" viewBox="0 0 24 24" {...props}>
+	<g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1">
+		<path fill="currentColor" stroke-width="2" d="M7 6c0 6.08 4.92 11 11 11c0.53 0 1.05 -0.04 1.56 -0.11c-1.61 2.47 -4.39 4.11 -7.56 4.11c-4.97 0 -9 -4.03 -9 -9c0 -3.17 1.64 -5.95 4.11 -7.56c-0.07 0.51 -0.11 1.03 -0.11 1.56Z">
+			<animate fill="freeze" attributeName="d" dur="0.6s" values="M7 28c0 6.08 4.92 11 11 11c0.53 0 1.05 -0.04 1.56 -0.11c-1.61 2.47 -4.39 4.11 -7.56 4.11c-4.97 0 -9 -4.03 -9 -9c0 -3.17 1.64 -5.95 4.11 -7.56c-0.07 0.51 -0.11 1.03 -0.11 1.56Z;M7 6c0 6.08 4.92 11 11 11c0.53 0 1.05 -0.04 1.56 -0.11c-1.61 2.47 -4.39 4.11 -7.56 4.11c-4.97 0 -9 -4.03 -9 -9c0 -3.17 1.64 -5.95 4.11 -7.56c-0.07 0.51 -0.11 1.03 -0.11 1.56Z" />
+		</path>
+		<g fill="none" stroke-dasharray="4" stroke-dashoffset="4">
+			<path d="M12 5h1.5M12 5h-1.5M12 5v1.5M12 5v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="0.7s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M17 11h1.5M17 11h-1.5M17 11v1.5M17 11v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="1.37s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M20 5h1.5M20 5h-1.5M20 5v1.5M20 5v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="2.04s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M12 4h1.5M12 4h-1.5M12 4v1.5M12 4v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="2.71s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M18 12h1.5M18 12h-1.5M18 12v1.5M18 12v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="3.38s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M19 4h1.5M19 4h-1.5M19 4v1.5M19 4v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="4.05s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M13 4h1.5M13 4h-1.5M13 4v1.5M13 4v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="4.72s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M19 11h1.5M19 11h-1.5M19 11v1.5M19 11v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="5.39s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+			<path d="M20 5h1.5M20 5h-1.5M20 5v1.5M20 5v-1.5">
+				<animate attributeName="stroke-dashoffset" begin="6.06s" dur="6s" keyTimes="0;0.08;0.22;0.3;1" repeatCount="indefinite" values="4;0;0;4;4" />
+			</path>
+		</g>
+	</g>
+</svg>`,
+};
